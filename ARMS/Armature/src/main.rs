8dbd1003c4eb36d0ffda6aff69e467a1988f0dbe
Dillon Flohr <dillonflohr@gmail.com>
2018-09-05 11:07:18 -0500

    reformmated main to get ready to call the upcoming interpreter, parser, and scanner

--- ARMS/Armature/src/main.rs
+++ ARMS/Armature/src/main.rs
@@ -6,24 +6,53 @@ use std::fs::File;
 use std::io::Read;
 use std::env;
 
+mod scanner;
+mod parser;
+mod interpreter;
+
+fn create(file_contents: String, file_name: &String) {
+    println!("Creating .cpp ODE file...");
+    let split_file_name: Vec<&str> = file_name.split('.').collect();
+    let new_file_name = format!("{}{}{}", split_file_name[0],"_ODE", ".cpp");
+    let new_file_location = format!("{}{}", "./", new_file_name);
+    File::create(&new_file_location).expect("Error during file creation.");
+
+    //interpreter write to the file here...
+    fs::write(new_file_location, file_contents).expect("Unable to write file");
+}
+
+fn get_file_contents(file_path: &String) -> String {
+    let mut file = File::open(file_path).expect("Could not open file");
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).expect("Could not read file into string");
+
+    return file_contents;
+}
+
+fn run_promt() {
+    println!("Enter name of ARMS file to interpret: ");
+    let mut file_path = String::new();
+	io::stdin().read_line(&mut file_path).expect("Need to input a file path!");
+
+    let file_contents = get_file_contents(&file_path);
+
+    create(file_contents, &file_path);
+}
+
+fn run_file(file_path: &String) {
+    let file_contents = get_file_contents(&file_path);
+
+    create(file_contents, &file_path);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() > 1, "Please provide a file name as an argument.");
 
-    //Get file name
-    let file_name = &args[1];
+    match args.len() {
+        1 => run_promt(),
+        2 => run_file(&args[1]),
+        _ => println!("Please run with either no arguments or with one specifying the file path.")
+    }
 
-    //open file and get the contents
-    let file_location_and_name = format!("{}{}","./input/", file_name.trim());
-    let mut file_to_read = File::open(file_location_and_name).expect("File not found!");
-	let mut file_contents = String::new();
-	file_to_read.read_to_string(&mut file_contents).expect("Something went wrong while reading the file.");
-    
-    //Create file contents and put in ./output/file_name
-    println!("Creating .cpp file...");
-    let split_file_name: Vec<&str> = file_name.split('.').collect();
-    let new_file_name = format!("{}{}", split_file_name[0], ".cpp");
-    let new_file_location = format!("{}{}", "./output/", new_file_name);
-    File::create(&new_file_location).expect("Error during file creation.");
-    fs::write(new_file_location, file_contents).expect("Unable to write file");
+    println!("Successfully created interpreted files!")
 }
